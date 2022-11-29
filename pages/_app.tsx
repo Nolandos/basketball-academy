62530bd9b3db578e667d0625d00b53318637b786
Łukasz Nolka <nolkalukasz@gmail.com>
2022-11-29 14:38:48 +0100

    FA-1: Add support for material ui

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,17 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import '../styles/globals.css';
 import type {AppProps} from 'next/app';
+import {appWithTranslation} from 'next-i18next';
 
-const App = ({Component, pageProps}: AppProps) => <Component {...pageProps} />;
+const App = ({Component, pageProps}: AppProps) => {
+    useEffect(() => {
+        // Remove the server-side injected CSS.
+        const jssStyles = document.querySelector('#jss-server-side');
+        if (jssStyles) {
+            jssStyles?.parentElement?.removeChild(jssStyles);
+        }
+    }, []);
+    return <Component {...pageProps} />;
+};
 
-export default App;
+export default appWithTranslation(App);
